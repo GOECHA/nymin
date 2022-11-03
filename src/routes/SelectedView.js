28e9ff8df504c7d581e377b3e44a3c58ec9a046e
Chantal Goethals <chantalgoethals@gmail.com>
2022-11-03 01:27:54 -0600

    Feat: format single article

--- src/routes/SelectedView.js
+++ src/routes/SelectedView.js
@@ -9,14 +9,6 @@ import "./SelectedView.css";
 const SelectedView = () => {
   const globals = useContext(AppContext)
 
- const upDate = () => {
- return globals.article.published_date.slice(
-  0,
-  globals.published_date.length - 15
-)}
-
-console.log(globals.article.published_date)
-// 
 
   return (
     <>
@@ -24,24 +16,22 @@ console.log(globals.article.published_date)
     <div className="selected-container">
       <div className="sel-header-wrapper">
         <h1 className="sel-header">{globals.article.title}</h1>
+        <div className="line"></div>
+      <div className="sel-date-wrapper">
+        <p className="sel-date">{globals.article.published_date}</p>
       </div>
-      <div className="section-container">
+      </div>
+      <div className="sel-section-container">
         <p className="sel-section">{globals.article.section}</p>
       </div>
+      <div className="sel-cent-container">
       <div className="sel-img-wrapper">
         <img className="selected-image" src={globals.article.multimedia[0].url} alt="selected" />
-        {/* <p className="sel-description">{globals.article.description}</p> */}
+       </div>
          <div className="sel-description-wrapper">
         <p className="sel-description">{globals.article.abstract}</p>
-       </div>
       </div>
-      <div className="sel-date-wrapper">
-        <p className="sel-date">{upDate}</p>
       </div>
-      {/* <div className="sel-description-wrapper">
-        <p className="sel-description">{globals.article.description}</p>
-      </div> */}
-      
     </div>
     </>
   );
