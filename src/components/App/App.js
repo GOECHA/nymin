cf00fbd500f480e77942143826373f0399389844
Chantal Goethals <chantalgoethals@gmail.com>
2022-11-03 18:02:51 -0600

    refactor: add .env

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,7 +14,6 @@ function App() {
 const [article, setArticle] = useState('')
 const [data, setData] = useState([]);
 const [loading, setLoading] = useState(false);
-// const [error, setError] = useState(null);
 const [search, setSearch] = useState([])
 
 
--- src/routes/SelectedView.js
+++ src/routes/SelectedView.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
 import AppContext from "../components/AppContext.js";
 import Header from "../components/Header/Header.js";
 import "./SelectedView.css";
